3e36249e5541d0f82644ad29bc0743656d7abf0d
疯味十三君 <a664554724@qq.com>
2019-12-22 16:25:19 +0800

    学少何

---
+++ List-FirstWork/IList.cs
@@ -0,0 +1,49 @@
+﻿/* 
+ * 凡路游戏部作业 List
+ * 简述：实现下面的IList接口，完成一个数据结构，实现可变长度数组
+ * 一开始默认长度为8，提供一个指定初始Count的构造方法和一个无参构造方法
+ * 当Insert的时候Count超过了Capacity时，Capacity翻倍
+ * 作者：WJL
+ * 版权所有，翻版不究
+ */
+namespace Final.FKGame.Study._001_List
+{
+    public interface IList<T>
+    {
+        /// <summary>
+        /// 存放数据数量
+        /// </summary>
+        int Count { get; set; }
+
+        /// <summary>
+        /// List 最大容量
+        /// </summary>
+        int Capacity { get; }
+
+        /// <summary>
+        /// 提供读取和编辑列表中的项的方法
+        /// </summary>
+        T this[int index] { get; set; }
+
+        /// <summary>
+        /// 搜索指定的对象，并返回第一个匹配项的索引
+        /// 如果项不在列表中，返回-1
+        /// </summary>
+        int IndexOf(T value);
+
+        /// <summary>
+        /// 插入一个值到列表末尾
+        /// </summary>
+        void Add(T value);
+
+        /// <summary>
+        /// 移除索引位置的项，其他项向前填补空缺
+        /// </summary>
+        void RemoveAt(int index);
+
+        /// <summary>
+        /// 使用默认比较器对所有元素进行排序
+        /// </summary>
+        void Sort();
+    }
+}
---
+++ List-FirstWork/OwnList.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Final.FKGame.Study._001_List
+{
+    public class OwnList<T> : IList<T>
+    {
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= Count - 1)
+                {
+                    return array[index];
+                }
+                else
+                {
+                    throw new Exception("Index out of range!");
+                }
+                
+            }
+            set
+            {
+                if (index >= 0 && index <= Count - 1)
+                {
+                    array[index] = value;
+                }
+                else
+                {
+                    throw new Exception("Index out of range!");
+                }
+            }
+        }
+        private T[] array;
+        private int count = 0;
+        public OwnList()
+        {
+            array = new T[0];
+        }
+
+        public OwnList(int capacity)
+        {
+            if (capacity >= 0)
+            {
+                array = new T[capacity];
+            }
+            else
+            {
+                throw new Exception("Wrong Capacity!");
+            }
+        }
+
+        public int Count { get { return count; } set { count = value; } }
+
+        public int Capacity { get { return array.Length; } }
+
+        public int IndexOf(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (object.Equals(value, array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        public int LastIndexOf(T value)
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                if (object.Equals(value, array[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        public void Add(T value)
+        {
+            if (Count == Capacity)
+            {
+                if (Capacity == 0)
+                {
+                    array = new T[8];
+                }
+                else
+                {
+                    T[] newArray = new T[Capacity * 2];
+                    Array.Copy(array, newArray, Count);
+                    array = newArray;
+                }
+            }
+
+            array[Count] = value;
+            Count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index <= Count - 1)
+            {
+                //List`1.cs中的原生移除方法
+                //Array.Copy((Array)array, index + 1, (Array)array, index , Count - index);
+                
+                //普通方法
+                for (int i = index + 1; i < Count; i++)
+                {
+                    array[i - 1] = array[i];
+                }
+                count--;
+            }
+            else
+            {
+                throw new Exception("Index out of range!");
+            }
+            
+        }
+
+        public void Insert(int index, T value)
+        {
+            if (index >= 0 && index <= Count - 1)
+            {
+                if (Count == Capacity)
+                {
+                    T[] newArray = new T[Capacity * 2];
+                    Array.Copy(array, newArray, Count);
+                    array = newArray;
+                }
+
+                for (int i = Count - 1; i >= index; i--)
+                {
+                    array[i + 1] = array[i];
+                }
+
+                count++;
+                array[index] = value;
+            }
+            else
+            {
+                throw new Exception("Index out of range!");
+            }
+        }
+
+        public void Sort()
+        {
+            int j;
+            T temp;
+            for (int i = 0; i < Count; i++)
+            {
+                j = i;
+                for (int k = i + 1; k < Count; k++)
+                {
+                    if (array[j].GetHashCode() > array[k].GetHashCode())
+                    {
+                        j = k;
+                    }
+                }
+
+                if (j != i)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
